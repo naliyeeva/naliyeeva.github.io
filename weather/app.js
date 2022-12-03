57b1d85451d9bb32e93ea41b9a09ed445b676c24
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2022-12-03 18:11:32 +0400

    added container for weather and api for country name

--- weather/app.js
+++ weather/app.js
@@ -5,6 +5,16 @@ App = {
         App.userInput.addEventListener('change', this.changeUserInput)
         App.getWeather.addEventListener('click', this.getWeatherFromApi)
     },
+
+    degreeToDirection(degree){
+        const deg =  Math.floor((degree / 45) + 0.5);
+        const directions = ["N","NE","E", "SE","S","SW","W","NW"];
+        return directions[(deg % 8)]
+    },
+    tempConversion(kelvin) {
+        kelvin = parseFloat(kelvin);
+        return kelvin - 273.15;
+    },
     changeUserInput() {
         let userInputType = document.getElementById('userInput').value
 
@@ -31,6 +41,23 @@ App = {
         let url = userInputType === 'latlong' || userInputType === 'geo'
             ? `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=14d29a796f48584f82e8287e911bc6a9`
             : `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=14d29a796f48584f82e8287e911bc6a9`
+
+
+        if(userInputType === 'city') {
+            let country = fetch('https://countriesnow.space/api/v0.1/countries/population/cities', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ city: city }),
+            })
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
 
         fetch(url)
             .then(res => {
